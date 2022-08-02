309c3a4d446dc02d68314ce3f8c5277694390c90
Matthew Batchelder <borkweb@gmail.com>
2022-08-02 14:18:15 -0400

    Diff base and HEAD

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ fi
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 if [ "${INPUT_ONLY_SNIFF_DIFF}" = true ]; then
-	php /usr/local/bin/phpcs.phar --standard=${INPUT_STANDARD} --extensions=php --report=checkstyle -q $(git diff --name-only $GITHUB_HEAD_REF $GITHUB_BASE_REF | grep "\.php") \
+	php /usr/local/bin/phpcs.phar --standard=${INPUT_STANDARD} --extensions=php --report=checkstyle -q $(git diff --name-only $GITHUB_BASE_REF..HEAD | grep "\.php") \
 	| reviewdog -name=PHP_CodeSniffer -f=checkstyle -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
 else
 	php /usr/local/bin/phpcs.phar --standard=${INPUT_STANDARD} --extensions=php --report=checkstyle -q ${INPUT_TARGET_DIRECTORY} \

a9a5b1408538ae2dd1a4d71aeb5d45d63aebb2ea
Matthew Batchelder <borkweb@gmail.com>
2022-08-04 16:03:30 -0400

    phpcs the thing and pipe to reviewdog

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,18 +7,13 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-echo "-------- All files:"
-git diff --name-only --diff-filter=ACMRTUXB origin/${GITHUB_BASE_REF}..origin/${GITHUB_HEAD_REF}
 echo "-------- PHP files:"
 git diff --name-only --diff-filter=ACMRTUXB origin/${GITHUB_BASE_REF}..origin/${GITHUB_HEAD_REF} | grep -E \.php$
 echo "--------"
 
 if [ "${INPUT_ONLY_SNIFF_DIFF}" = true ]; then
-	php vendor/bin/phpcs --standard=${INPUT_STANDARD} --extensions=php --report=sniff.xml
-	cat sniff.xml
-	cat sniff.xml | reviewdog -name=PHP_CodeSniffer -f=checkstyle -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
-	#php /usr/local/bin/phpcs.phar --standard=${INPUT_STANDARD} --extensions=php --report=checkstyle -q $(git diff --name-only --diff-filter=ACMRTUXB origin/${GITHUB_BASE_REF}..origin/${GITHUB_HEAD_REF} | grep -E \.php$) \
-	#| reviewdog -name=PHP_CodeSniffer -f=checkstyle -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
+	php vendor/bin/phpcs --standard=${INPUT_STANDARD} --extensions=php --report=checkstyle -q $(git diff --name-only --diff-filter=ACMRTUXB origin/${GITHUB_BASE_REF}..origin/${GITHUB_HEAD_REF} | grep -E \.php$) \
+	| reviewdog -name=PHP_CodeSniffer -f=checkstyle -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
 else
 	php vendor/bin/phpcs --standard=${INPUT_STANDARD} --extensions=php --report=checkstyle -q ${INPUT_TARGET_DIRECTORY} \
 	| reviewdog -name=PHP_CodeSniffer -f=checkstyle -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'

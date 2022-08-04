c6b07f0932b6b62c79cfea43c73c44fe9a94f67c
Matthew Batchelder <borkweb@gmail.com>
2022-08-04 16:00:45 -0400

    Execute phpcs from the vendor dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,12 +14,12 @@ git diff --name-only --diff-filter=ACMRTUXB origin/${GITHUB_BASE_REF}..origin/${
 echo "--------"
 
 if [ "${INPUT_ONLY_SNIFF_DIFF}" = true ]; then
-	php /usr/local/bin/phpcs.phar --standard=${INPUT_STANDARD} --extensions=php --report=sniff.xml
+	php vendor/bin/phpcs --standard=${INPUT_STANDARD} --extensions=php --report=sniff.xml
 	cat sniff.xml
 	cat sniff.xml | reviewdog -name=PHP_CodeSniffer -f=checkstyle -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
 	#php /usr/local/bin/phpcs.phar --standard=${INPUT_STANDARD} --extensions=php --report=checkstyle -q $(git diff --name-only --diff-filter=ACMRTUXB origin/${GITHUB_BASE_REF}..origin/${GITHUB_HEAD_REF} | grep -E \.php$) \
 	#| reviewdog -name=PHP_CodeSniffer -f=checkstyle -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
 else
-	php /usr/local/bin/phpcs.phar --standard=${INPUT_STANDARD} --extensions=php --report=checkstyle -q ${INPUT_TARGET_DIRECTORY} \
+	php vendor/bin/phpcs --standard=${INPUT_STANDARD} --extensions=php --report=checkstyle -q ${INPUT_TARGET_DIRECTORY} \
 	| reviewdog -name=PHP_CodeSniffer -f=checkstyle -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
 fi
